b42a86873a9df031dd323a68adcfb5b13f22211d
Magdalena <57455351+BMagda2911@users.noreply.github.com>
2021-02-04 16:54:58 +0100

    swapped own trial code with plugin's search function

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
 baguetteBox.run('.gallery');
 
-function search_picture() { 
-    let input = document.getElementById('searchbar').value 
-    input=input.toLowerCase(); 
-    let x = document.getElementsByClassName('pictures'); 
-     // pictures in line 6 should probably be changes to the class name "card" 
-    for (i = 0; i < x.length; i++) {  
-        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-            x[i].style.display="none"; 
-        } 
-        else { 
-            x[i].style.display="list-item";                  
-        } 
-    } 
-} 
+const search = new Filter('search', 'data-caption');
